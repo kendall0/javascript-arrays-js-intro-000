124305b17a3c43037a021118e733f93b35df0947
Kendall George <kendallanngeorge@gmail.com>
2021-02-26 19:01:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,2 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-// adds an element to the beginning of an array
-function addElementToBeginningOfArray(array, 'foo'); {
-  const array = [1];
-}
-array;

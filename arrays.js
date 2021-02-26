e0559d56639bf47477533b352f358ed9446ed24a
Kendall George <kendallanngeorge@gmail.com>
2021-02-26 19:10:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  const array = [1]
+  const array = [1];
+  addElementToBeginningOfArray(array, 'foo');
 }
-= [1];
-[...addElementToBeginningOfArray, 'foo'];
